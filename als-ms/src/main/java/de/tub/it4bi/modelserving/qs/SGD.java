7b761399922289054966d40707dfd06f716ba5a2
Ziyad Muhammed <mmziyad@gmail.com>
2017-07-20 15:58:10 +0200

    [Update] SGD with global model query in SGD step

--- als-ms/src/main/java/de/tub/it4bi/modelserving/qs/SGD.java
+++ als-ms/src/main/java/de/tub/it4bi/modelserving/qs/SGD.java
@@ -3,9 +3,8 @@ package de.tub.it4bi.modelserving.qs;
 import de.tub.it4bi.modelserving.utils.QueryClientHelper;
 import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.JobID;
-import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
 import org.apache.flink.api.common.time.Time;
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
@@ -16,6 +15,7 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.ml.common.WeightVector;
 import org.apache.flink.ml.math.DenseVector;
@@ -34,7 +34,7 @@ import java.util.Optional;
 /**
  * Perform online updates to the Recommender model using SGD
  */
-public class SGDKafkaProducer {
+public class SGD {
     public static void main(String[] args) {
         // create execution environment
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -44,28 +44,45 @@ public class SGDKafkaProducer {
         // add the stream source
         TextInputFormat io = new TextInputFormat(new Path(params.getRequired("input")));
         io.setNestedFileEnumeration(true);
-        DataStream<String> messageStream = env.readFile(io, params.getRequired("input"),
-                FileProcessingMode.PROCESS_CONTINUOUSLY, params.getLong("interval", 60000));
+
+        // set the process as a continuous stream job or process once job
+        DataStream<String> messageStream = null;
+        if (params.getRequired("mode").equals("continuous")) {
+            messageStream = env.readFile(
+                    io,
+                    params.getRequired("input"),
+                    FileProcessingMode.PROCESS_CONTINUOUSLY,
+                    params.getLong("interval", 60000));
+        } else if (params.getRequired("mode").equals("once")) {
+            messageStream = env.readFile(
+                    io,
+                    params.getRequired("input"),
+                    FileProcessingMode.PROCESS_ONCE,
+                    params.getLong("interval", 60000));
+        } else {
+            System.err.println("Invalid mode. Specify --mode [continuous|once] ");
+            System.exit(-1);
+        }
 
         // Apply SGD and update the model
         DataStream<String> modelUpdates = messageStream
-                .map(new InputParser(params))   // parse input rating data
-                .keyBy(0)
-                .map(new CurrentLatentFactors(params)) // retrieve current model parameters
-                .map(new SGDRecommender(params))       // apply SGD
-                .flatMap(new ModelUpdater());          // update model with the new parameters
+                .map(new InputParser(params))// parse input rating data
+                .flatMap(new SGDStep(params));
 
-        // write updated model to Kafka topic
-        FlinkKafkaProducer010Configuration myProducerConfig = FlinkKafkaProducer010.writeToKafkaWithTimestamps(
-                modelUpdates,                     // input stream
-                params.getRequired("topic"), // target topic
-                new SimpleStringSchema(),         // serialization schema
-                params.getProperties());          // custom configuration for KafkaProducer (including broker list)
+        if (params.getRequired("outputMode").equals("kafka")) {
+            // write updated model to Kafka topic
+            FlinkKafkaProducer010Configuration myProducerConfig = FlinkKafkaProducer010.writeToKafkaWithTimestamps(
+                    modelUpdates,                     // input stream
+                    params.getRequired("topic"), // target topic
+                    new SimpleStringSchema(),         // serialization schema
+                    params.getProperties());          // custom configuration for KafkaProducer (including broker list)
 
-        // the following is necessary for at-least-once delivery guarantee
-        myProducerConfig.setLogFailuresOnly(false);   // "false" by default
-        myProducerConfig.setFlushOnCheckpoint(true);  // "false" by default
-
+            // the following is necessary for at-least-once delivery guarantee
+            myProducerConfig.setLogFailuresOnly(false);   // "false" by default
+            myProducerConfig.setFlushOnCheckpoint(true);  // "false" by default
+        } else if (params.getRequired("outputMode").equals("hdfs") && params.has("outputPath")) {
+            modelUpdates.writeAsText(params.get("outputPath"), FileSystem.WriteMode.OVERWRITE);
+        }
         try {
             env.execute("[ALS] online-updates using SGD");
         } catch (Exception e) {
@@ -74,23 +91,40 @@ public class SGDKafkaProducer {
     }
 
     /**
-     * Retrieves the current latent factors from the ALS model served using queryable state
+     * Parse the rating data, to derive userID, itemID and rating
      */
-    private static class CurrentLatentFactors extends
-            RichMapFunction<Tuple3<Integer, Integer, Double>,
-                    Tuple3<Tuple2<Integer, WeightVector>, Tuple2<Integer, WeightVector>, Double>> {
-        QueryClientHelper<String, Tuple2<String, String>> client;
+    private static class InputParser implements MapFunction<String, Tuple3<Integer, Integer, Double>> {
         ParameterTool params;
+
+        public InputParser(ParameterTool params) {
+            this.params = params;
+        }
+
+        @Override
+        public Tuple3<Integer, Integer, Double> map(String value) throws Exception {
+            // values are expected in the order: userID, itemID, rating
+            String tokens[] = value.split(params.get("fieldDelimiter", "\t"));
+            return new Tuple3<>(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]), Double.parseDouble(tokens[2]));
+        }
+    }
+
+    /**
+     * Apply SGD and produce updated vectors to be sent to Kafka
+     */
+
+    private static class SGDStep extends RichFlatMapFunction<Tuple3<Integer, Integer, Double>, String> {
+        ParameterTool params;
+        QueryClientHelper<String, Tuple2<String, String>> client;
         String userMeanFactors;
         String itemMeanFactors;
+        StringBuilder sb = new StringBuilder();
 
-        public CurrentLatentFactors(ParameterTool params) {
+        public SGDStep(ParameterTool params) {
             this.params = params;
         }
 
         @Override
         public void open(Configuration parameters) throws Exception {
-
             final JobID jobId = JobID.fromHexString(params.getRequired("jobId"));
             final String jobManagerHost = params.get("jobManagerHost", "localhost");
             final int jobManagerPort = params.getInt("jobManagerPort", 6123);
@@ -108,14 +142,26 @@ public class SGDKafkaProducer {
             // Note: In the current version, mean vectors will be queried only at the start of this job.
             userMeanFactors = client.queryState("ALS_MODEL", "MEAN-U")
                     .map(x -> x.f1).orElse(params.get("userMean"));
-
-            userMeanFactors = client.queryState("ALS_MODEL", "MEAN-I")
+            itemMeanFactors = client.queryState("ALS_MODEL", "MEAN-I")
                     .map(x -> x.f1).orElse(params.get("itemMean"));
+
+            if (userMeanFactors == null || itemMeanFactors == null) {
+                throw new IllegalStateException("Unable to load User mean or item mean factors.");
+            }
         }
 
         @Override
-        public Tuple3<Tuple2<Integer, WeightVector>, Tuple2<Integer, WeightVector>, Double> map
-                (Tuple3<Integer, Integer, Double> value) throws Exception {
+        public void close() throws Exception {
+            client.close();
+        }
+
+        @Override
+        public void flatMap(Tuple3<Integer, Integer, Double> value, Collector<String> out) throws Exception {
+
+            // hyper parameters for SGD online learning
+            final double learningRate = params.getDouble("learningRate", 0.1);
+            final double userReg = params.getDouble("userRegularization", 0.0);
+            final double itemReg = params.getDouble("itemRegularization", 0.0);
 
             int userID = value.f0;
             int itemID = value.f1;
@@ -123,62 +169,12 @@ public class SGDKafkaProducer {
 
             WeightVector uwv = extractWeightVector(userID, "-U", userMeanFactors, 0.0);
             WeightVector iwv = extractWeightVector(itemID, "-I", itemMeanFactors, 0.0);
-            return new Tuple3<>(new Tuple2<>(userID, uwv), new Tuple2<>(itemID, iwv), rating);
-        }
 
-        private WeightVector extractWeightVector(int id, String suffix, String meanFactors, double initialBias) {
-            String queryKey = id + suffix;
-            Optional<Tuple2<String, String>> output = null;
 
-            try {
-                output = client.queryState("ALS_MODEL", queryKey);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            String result = output.map(x -> x.f1).orElse(meanFactors);
-            double[] latentFactors = Arrays
-                    .stream(result.split(";"))
-                    .mapToDouble(Double::parseDouble).toArray();
-
-            // TODO: If the model contains bias, use that bias instead of initial bias
-            return new WeightVector(new DenseVector(latentFactors), initialBias);
-        }
-
-        @Override
-        public void close() throws Exception {
-            client.close();
-        }
-    }
-
-    /**
-     * Applies the SGD steps to update latent factors
-     */
-    private static class SGDRecommender implements
-            MapFunction<Tuple3<Tuple2<Integer, WeightVector>, Tuple2<Integer, WeightVector>, Double>,
-                    Tuple2<Tuple2<Integer, WeightVector>, Tuple2<Integer, WeightVector>>> {
-        ParameterTool params;
-
-        public SGDRecommender(ParameterTool params) {
-            this.params = params;
-        }
-
-        @Override
-        public Tuple2<Tuple2<Integer, WeightVector>, Tuple2<Integer, WeightVector>> map(
-                Tuple3<Tuple2<Integer, WeightVector>, Tuple2<Integer, WeightVector>, Double> value) throws Exception {
-
-            // hyper parameters for SGD online learning
-            final double learningRate = params.getDouble("learningRate", 0.1);
-            final double userReg = params.getDouble("userRegularization", 0.0);
-            final double itemReg = params.getDouble("itemRegularization", 0.0);
-
-            int userID = value.f0.f0;
-            int itemID = value.f1.f0;
-            double rating = value.f2;
-            double userBias = value.f0.f1.intercept();
-            double itemBias = value.f1.f1.intercept();
-            Vector userVector = value.f0.f1.weights();
-            Vector itemVector = value.f1.f1.weights();
+            double userBias = uwv.intercept();
+            double itemBias = iwv.intercept();
+            Vector userVector = uwv.weights();
+            Vector itemVector = iwv.weights();
 
             double prediction = userVector.dot(itemVector);
             double error = rating - prediction;
@@ -199,45 +195,7 @@ public class SGDKafkaProducer {
                 itemVector.update(i, updatedLatentFactor);
             }
 
-            return new Tuple2<>(new Tuple2<>(userID, new WeightVector(userVector, userBias)),
-                    new Tuple2<>(itemID, new WeightVector(itemVector, itemBias)));
-        }
-    }
-
-    /**
-     * Parse the rating data, to derive userID, itemID and rating
-     */
-    private static class InputParser implements MapFunction<String, Tuple3<Integer, Integer, Double>> {
-        ParameterTool params;
-
-        public InputParser(ParameterTool params) {
-            this.params = params;
-        }
-
-        @Override
-        public Tuple3<Integer, Integer, Double> map(String value) throws Exception {
-            // values are expected in the order: userID, itemID, rating
-            String tokens[] = value.split(params.get("fieldDelimiter", ","));
-            return new Tuple3<>(Integer.parseInt(tokens[0]),
-                    Integer.parseInt(tokens[1]),
-                    Double.parseDouble(tokens[2]));
-        }
-    }
-
-    /**
-     * Prepare the output weight vectors in the model schema
-     */
-    private static class ModelUpdater implements FlatMapFunction<Tuple2<Tuple2<Integer, WeightVector>,
-            Tuple2<Integer, WeightVector>>, String> {
-        StringBuilder sb = new StringBuilder();
-
-        @Override
-        public void flatMap(Tuple2<Tuple2<Integer, WeightVector>, Tuple2<Integer, WeightVector>> value,
-                            Collector<String> out) throws Exception {
-
             // prepare user record
-            Vector userVector = value.f0.f1.weights();
-            int userID = value.f0.f0;
             sb.setLength(0);
             for (int i = 0; i < userVector.size(); i++) {
                 sb.append(userVector.apply(i));
@@ -248,8 +206,6 @@ public class SGDKafkaProducer {
             out.collect(userRecord);
 
             // prepare item record
-            Vector itemVector = value.f1.f1.weights();
-            int itemID = value.f1.f0;
             sb.setLength(0);
             for (int i = 0; i < itemVector.size(); i++) {
                 sb.append(itemVector.apply(i));
@@ -258,6 +214,22 @@ public class SGDKafkaProducer {
             // TODO: Add bias to the item record in the model
             String itemRecord = itemID + ",I," + sb.toString();
             out.collect(itemRecord);
+        }
+
+        private WeightVector extractWeightVector(int id, String suffix, String meanFactors, double bias) {
+            String queryKey = id + suffix;
+            Optional<Tuple2<String, String>> output = null;
+
+            try {
+                output = client.queryState("ALS_MODEL", queryKey);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            String result = output.map(x -> x.f1).orElse(meanFactors);
+            double[] latentFactors = Arrays.stream(result.split(";")).mapToDouble(Double::parseDouble).toArray();
+            //TODO: If the model contains bias, use that bias instead of user provided bias
+            return new WeightVector(new DenseVector(latentFactors), bias);
         }
     }
 }
